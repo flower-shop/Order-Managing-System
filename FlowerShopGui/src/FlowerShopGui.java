f55246b7c21924b2eaabd4f9d0ba94787ce19ffc
James <James@10.0.0.166>
2019-05-01 16:57:12 -0400

    Removed connection fields from FlowerShopGui.java. Generated svID

--- FlowerShopGui/src/FlowerShopGui.java
+++ FlowerShopGui/src/FlowerShopGui.java
@@ -1,20 +1,11 @@
-import java.sql.Connection;
-import java.sql.Statement;
-
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
 import view.MainWindow;
-//@formatter:off
+
 public class FlowerShopGui extends JFrame {
-	private static final long serialVersionUID = 1L;
-
-	static final String JDBC_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-	static final String DB_URL = "jdbc:derby:FlowershopData;create=true";
-
-	static Connection connection = null;
-	static Statement statement = null;
+	private static final long serialVersionUID = 8803195407449611129L;
 
 	private FlowerShopGui() {
 		this.setLookAndFeel();
