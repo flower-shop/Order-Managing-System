aa041bd6ef924c2b08fcf84374f9a989b07de87a
James <James@10.0.0.166>
2019-05-08 10:50:41 -0400

    Updated fields in OrderDTO to match database columns

--- FlowerShopGui/src/dto/OrderDTO.java
+++ FlowerShopGui/src/dto/OrderDTO.java
@@ -2,124 +2,156 @@ package dto;
 import java.util.Date;
 
 public class OrderDTO {
-	private int orderNumber;
-	private double totalCost;
-	private String phoneNumber;
-	private String flowerType;
-	private String address;
 	private Date dateOfOrder;
-	private Date dateOfDelivery;
-	private String isDelivered;
+	private String firstName;
+	private String lastName;
+	private String flowerType;
+	private int flowerQty;
+	private String accessory;
+	private String arrangementType;
+	private String address;
+	private Date deliveryDate;
+	private double totalCost;
+	private String hasCard;
 	private String cardText;
 	private String isPaidFor;
+	private String isDelivered;
+	private int orderNumber;
 
-	public OrderDTO(int orderNumber, double totalCost, String phoneNumber, String flowerType, 
-			String address, Date dateOfOrder, Date dateOfDelivery, String isDelivered, String cardText, 
-			String isPaidFor) {
-		this.orderNumber = orderNumber;
-		this.totalCost = totalCost;
-		this.phoneNumber = phoneNumber;
-		this.flowerType = flowerType;
-		this.address = address;
+	public OrderDTO(Date dateOfOrder, String firstName, String lastName, String flowerType, int flowerQty,
+			String accessory, String arrangementType, String address, Date deliveryDate, double totalCost,
+			String hasCard, String cardText, String isPaidFor, String isDelivered, int orderNumber) {
+
 		this.dateOfOrder = dateOfOrder;
-		this.dateOfDelivery = dateOfDelivery;
-		this.isDelivered = isDelivered;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.flowerType = flowerType;
+		this.flowerQty = flowerQty;
+		this.accessory = accessory;
+		this.arrangementType = arrangementType;
+		this.address = address;
+		this.deliveryDate = deliveryDate;
+		this.totalCost = totalCost;
+		this.hasCard = hasCard;
 		this.cardText = cardText;
 		this.isPaidFor = isPaidFor;
+		this.isDelivered = isDelivered;
+		this.orderNumber = orderNumber;
 	}
-	
-	public int getOrderNumber() {
-		return orderNumber;
-	}
-	
-	public double getTotalCost() {
-		return totalCost;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	
-	public String getFlowerType() {
-		return flowerType;
-	}
-	
-	/*public AccessoryDTO  getAccessory() {
-		return accessory;
-	}
-	
-	public String getArrangementTheme() {
-		return arrangementTheme;
-	}*/
-	public String getAddress() {
-		return address;
-	}
-
-	
 
 	public Date getDateOfOrder() {
 		return dateOfOrder;
 	}
 
-	public Date getDateOfDelivery() {
-		return dateOfDelivery;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public String getIsDelivered() {
-		return isDelivered;
+	public String getLastName() {
+		return lastName;
 	}
-	
+
+	public String getFlowerType() {
+		return flowerType;
+	}
+
+	public int getFlowerQty() {
+		return flowerQty;
+	}
+
+	public String getAccessory() {
+		return accessory;
+	}
+
+	public String getArrangementType() {
+		return arrangementType;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+	public Date getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public String getHasCard() {
+		return hasCard;
+	}
+
 	public String getCardText() {
 		return cardText;
 	}
-
 	public String getIsPaidFor() {
 		return isPaidFor;
 	}
-	public void setOrderNumber(int orderNumber) {
-		this.orderNumber = orderNumber;
+	public String getIsDelivered() {
+		return isDelivered;
+	}
+
+	public int getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setDateOfOrder(Date dateOfOrder) {
+		this.dateOfOrder = dateOfOrder;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setFlowerType(String flowerType) {
+		this.flowerType = flowerType;
+	}
+
+	public void setFlowerQty(int flowerQty) {
+		this.flowerQty = flowerQty;
+	}
+
+	public void setAccessory(String accessory) {
+		this.accessory = accessory;
+	}
+
+	public void setArrangementType(String arrangementType) {
+		this.arrangementType = arrangementType;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setDeliveryDate(Date deliveryDate) {
+		this.deliveryDate = deliveryDate;
 	}
 
 	public void setTotalCost(double totalCost) {
 		this.totalCost = totalCost;
 	}
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	
-	public void setFlowerType(String flowerType) {
-		this.flowerType = flowerType;
-	}
-	
-	/*public void setAccessory(AccessoryDTO accessory) {
-		this.accessory = accessory;
-	}
-	
-	public void setArrangementTheme(String arrangementTheme) {
-		this.arrangementTheme = arrangementTheme;
-	}*/
-	public void setAddress(String address) {
-		this.address = address;
+	public void setHasCard(String hasCard) {
+		this.hasCard = hasCard;
 	}
 
-	public void setDateOfOrder(Date dateOfOrder) {
-		this.dateOfOrder = dateOfOrder;
+	public void setCardText(String cardText) {
+		this.cardText = cardText;
 	}
 
-	public void setDateOfDelivery(Date dateOfDelivery) {
-		this.dateOfDelivery = dateOfDelivery;
+	public void setIsPaidFor(String isPaidFor) {
+		this.isPaidFor = isPaidFor;
 	}
 
-	public void setisDelivered(String isDelivered) {
+	public void setIsDelivered(String isDelivered) {
 		this.isDelivered = isDelivered;
 	}
-	
-	public void setCardText(String orderCardText) {
-		this.cardText = orderCardText;
-	}
 
-	public void setPaidFor(String isPaidFor) {
-		this.isPaidFor = isPaidFor;
+	public void setOrderNumber(int orderNumber) {
+		this.orderNumber = orderNumber;
 	}
 }
