7bf557622ebb4dcced234e0aca02cbf237ba8d46
James <James@10.0.0.166>
2019-05-12 22:16:48 -0400

    Delete MAX_ROW_COUNT from ViewConstants

--- FlowerShopGui/src/view/ViewConstants.java
+++ FlowerShopGui/src/view/ViewConstants.java
@@ -11,5 +11,4 @@ import java.awt.Color;
 public class ViewConstants {
 	public static Color BACKGROUND_PANEL_COLOR = new Color(Integer.parseInt("ffcbbc", 16));
 	public static Color BACKGROUND_TAB_COLOR = new Color(Integer.parseInt("bee8d9", 16));
-	public static Integer MAX_ROW_COUNT = 256;
 }
