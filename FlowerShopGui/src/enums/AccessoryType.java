40527152cb5fa2cbd264e969f400c1c222593913
James <James@10.0.0.166>
2019-05-12 20:05:42 -0400

    Added headers to enums package

--- FlowerShopGui/src/enums/AccessoryType.java
+++ FlowerShopGui/src/enums/AccessoryType.java
@@ -1,5 +1,11 @@
 package enums;
 
+/*
+ * File: AccessoryType.java
+ * Date: May 12, 2019
+ * Authors: Olga Samborski, Jasmine Jones, Scott Hussey, and James DeBenedictis
+ */
+
 public enum AccessoryType {
 	Vase, Wreath, Bouquet, None
 }
--- FlowerShopGui/src/enums/ArrangementTheme.java
+++ FlowerShopGui/src/enums/ArrangementTheme.java
@@ -1,5 +1,11 @@
 package enums;
 
+/*
+ * File: ArrangementTheme.java
+ * Date: May 12, 2019
+ * Authors: Olga Samborski, Jasmine Jones, Scott Hussey, and James DeBenedictis
+ */
+
 public enum ArrangementTheme {
 	Birthday, Funeral, Romantic, None
 }
