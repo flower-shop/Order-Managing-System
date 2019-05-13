0a9779274456d7728399129ec9537f0875e4b5bc
James <James@10.0.0.166>
2019-05-12 20:04:56 -0400

    Added header to util package

--- FlowerShopGui/src/util/CurrencyFormatter.java
+++ FlowerShopGui/src/util/CurrencyFormatter.java
@@ -2,6 +2,12 @@ package util;
 
 import java.text.NumberFormat;
 
+/*
+ * File: CurrencyFormatter.java
+ * Date: May 12, 2019
+ * Authors: Olga Samborski, Jasmine Jones, Scott Hussey, and James DeBenedictis
+ */
+
 public class CurrencyFormatter {
 
 	public static NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
